2ff4e964c360d1702ccb12413341a468f8cd80d1
pnayak99 <b517029@iiit-bh.ac.in>
2021-07-28 15:09:52 +0530

    Second commit

--- JavaGitProject/src/main/java/com/lti/demo/MyFile.java
+++ JavaGitProject/src/main/java/com/lti/demo/MyFile.java
@@ -1,8 +1,9 @@
 package com.lti.demo;
 
-public class Person {
+public class MyFile {
 	public static void main(String[] args) {
 		System.out.println("Pull implements");
+		System.out.println("Yes I got");
 	}
 	
 
